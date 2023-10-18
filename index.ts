3179beceaa5fbc439256d5f185083af662ab80f0
Mukul Jain <jainmukul1985@gmail.com>
2023-10-18 20:55:38 +0530

    adding routes

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ const server = Bun.serve({
   port: 3000,
   fetch(req) {
     const url =  new URL(req.url)
-    const body = figlet.textSync("video")
-    return new Response(body)
-    
+    if (url.pathname == '/') {
+      const body = figlet.textSync("video")
+      return new Response(body)
+    }
+
+    if (url.pathname === '/about') {
+      return new Response("About me!")
+    }
+
+    if (url.pathname === '/contact') {
+      return new Response("contact us!")
+    }
+
+    return new Response("404!")
   }
 })
 
